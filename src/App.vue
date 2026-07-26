9e90ce3537c45b25d22efc0afbf4cd85466b1043
Administrator <bai5_w@163.com>
2026-07-26 16:49:08 +0800

    feat: 移除element plus全局导入-配置国际化为中文

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-
+import { ElConfigProvider } from 'element-plus'
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 </script>
 
 <template>
-  <header>
-
-  </header>
-
+  <ElConfigProvider  -provider :locale="zhCn">
+      <RouterView />
+  </ElConfigProvider>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
-
 const app = createApp(App)
-app.use(ElementPlus, {
-  locale: zhCn,
-})
+
 app.use(createPinia())
 app.use(router)
 
