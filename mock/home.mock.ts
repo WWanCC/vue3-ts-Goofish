e4a14ed01bf3ee90a38fc6a303e6a5fba262ef68
Administrator <bai5_w@163.com>
2026-07-27 23:58:23 +0800

    feat: 测试游标连通

--- mock/home.mock.ts
+++ mock/home.mock.ts
@@ -5,22 +5,24 @@ export default defineMock({
   method: 'GET',
   delay: 900,
   body: {
-    data: [
-      {
-        id: 1,
-        title: '机械键盘，九成新',
-        price: 199,
-      },
-      {
-        id: 2,
-        title: '闲置显示器支架',
-        price: 89,
-      },
-      {
-        id: 3,
-        title: '全新未拆封鼠标',
-        price: 129,
-      },
-    ],
+    data: {
+      list: [
+        {
+          id: 1,
+          title: '机械键盘，九成新',
+          price: 199,
+        },
+        {
+          id: 2,
+          title: '闲置显示器支架',
+          price: 89,
+        },
+        {
+          id: 3,
+          title: '全新未拆封鼠标',
+          price: 129,
+        },
+      ],
+    },
   },
 })
