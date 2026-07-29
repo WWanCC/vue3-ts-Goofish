5619b1cb124f58b6ec56daedb22806741e54b6f8
Administrator <bai5_w@163.com>
2026-07-29 22:07:11 +0800

    feat: 调整HomeHereSection样式

--- src/modules/home/components/HomeHeroSection.vue
+++ src/modules/home/components/HomeHeroSection.vue
@@ -5,7 +5,10 @@ import PromoBanner from "./PromoBanner.vue";
 </script>
 
 <template>
-  <section class="mb-6 flex flex-col gap-3 xl:flex-row" aria-label="首页推荐区域">
+  <section
+    class="mb-6 flex flex-col gap-3 xl:h-[316px] xl:flex-row"
+    aria-label="首页推荐区域"
+  >
     <CategoryMenu />
 
     <PromoBanner />
