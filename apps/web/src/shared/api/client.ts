1e6e42a9c0ec2cdc7b204f485f9cb0b626897cab
Administrator <bai5_w@163.com>
2026-08-16 21:05:07 +0800

    feat: 调整前端跨域配置

--- apps/web/src/shared/api/client.ts
+++ apps/web/src/shared/api/client.ts
@@ -1,10 +1,5 @@
 import {hc} from 'hono/client'
 import type {AppType} from '@goofish/api'
 
-export const client = hc<AppType>('http://localhost:3000')
-client.api.home.products.$get({
-  query: {
-    limit: '12',
-    cursor: '12'
-  }
-})
+export const client = hc<AppType>('/')
+
