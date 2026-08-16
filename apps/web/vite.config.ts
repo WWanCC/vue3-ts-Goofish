808ac7556654d9f79dd0adf3bc601cbae3c4f0b7
Administrator <bai5_w@163.com>
2026-08-16 20:44:13 +0800

    feat: 修改配置，替换mock

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { mockDevServerPlugin } from 'vite-plugin-mock-dev-server'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import {mockDevServerPlugin} from 'vite-plugin-mock-dev-server'
 
 export default defineConfig({
   plugins: [
@@ -20,13 +20,20 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-    mockDevServerPlugin({
-      prefix: ['/api'],
-    }),
+    // mockDevServerPlugin({
+    //   prefix: ['/api'],
+    // }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      },
     },
   },
 })
