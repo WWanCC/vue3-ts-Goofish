78fbde2932f4dde8aece975c06f332e28ad2c8cf
Administrator <bai5_w@163.com>
2026-08-22 18:23:35 +0800

    feat(web): 完善响应类型

--- apps/web/src/modules/product/api/product.api.ts
+++ apps/web/src/modules/product/api/product.api.ts
@@ -1,16 +1,35 @@
 import {client} from '@/shared/api/client.ts'
+// 提供的 RPC 响应类型推导工具
+import type {InferResponseType} from 'hono/client'
 
-export async function getProductDetail(id: number) {
+//先把这个 RPC 请求方法单独拿出来
+const getProductDetailRequest = client.api.products[':id'].$get
 
-  const response = await client.api.products[':id'].$get({
+type ProductDetailResponse = InferResponseType<
+  typeof getProductDetailRequest,
+  200
+>
+
+type ProductDetailError = InferResponseType<
+  typeof getProductDetailRequest,
+  404
+>
+
+export async function getProductDetail(id: number): Promise<ProductDetailResponse> {
+
+
+
+  const response = await getProductDetailRequest({
     param: {
       id: String(id),
     },
   })
 
+
+
   // 后端明确可能返回 404。
   if (response.status === 404) {
-    const error = await response.json()
+    const error = (await response.json()) as ProductDetailError
     throw new Error(error.message)
   }
 
@@ -20,5 +39,5 @@ export async function getProductDetail(id: number) {
   }
 
   // 这里的商品数据类型由后端 AppType 推导。
-  return response.json()
+  return (await response.json()) as ProductDetailResponse
 }
